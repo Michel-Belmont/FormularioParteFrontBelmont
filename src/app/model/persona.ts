6292b096502630ef5a2cda132e0a901d65df5403
Michel-Belmont <gbelmontb001@alumno.uaemex.mx>
2021-10-31 17:17:12 -0600

    front

---
+++ src/app/model/persona.ts
@@ -0,0 +1,14 @@
+import { Pais } from "./pais";
+
+
+export class Persona{
+  idPersona!: number;
+  nombres!: string;
+  apellidos!: string;
+  edad!: number;
+  pais!: Pais;
+  sexo!: string;
+  fecha_de_Nacimiento!:string
+
+
+}
